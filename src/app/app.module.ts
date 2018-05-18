0fe1f0b134c85c13f2c97bd54222277d82ae87a7
Nic-Kofo <nickkofo@gmail.com>
2018-05-18 20:00:44 +0300

    made pipe for days

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StoreProvider } from '../providers/store/store';
 import { Api } from '../providers/api/api';
 import { HttpClientExt } from '../providers/http-client/http-client';
-
 
 @NgModule({
   declarations: [
--- src/pages/store/store.module.ts
+++ src/pages/store/store.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { StorePage } from './store';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { StorePage } from './store';
   ],
   imports: [
     IonicPageModule.forChild(StorePage),
+    PipesModule
   ],
 })
 export class StorePageModule {}
