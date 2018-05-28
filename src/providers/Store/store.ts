d85fe42b8e720151877cc2222c796364824e1624
Talagozis <talagozis8@gmail.com>
2018-05-28 20:59:36 +0300

    Implement StoreSubscriptionFilters class.

--- src/providers/Store/store.ts
+++ src/providers/Store/store.ts
@@ -1,25 +1,58 @@
 import { Injectable } from '@angular/core';
-
 import { StoreApi } from '../../models/api/Store';
 import { Api } from '../api/api';
 import { Observable } from 'rxjs/Observable';
-/*
-  Generated class for the StoreProvider provider.
+import { Subscription } from 'rxjs/Subscription';
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+type SubscriptionDelegate = (value: StoreApi[]) => void;
+
 @Injectable()
 export class StoreProvider {
 
   constructor(public api: Api<StoreApi>) { }
 
-   find(): Observable<StoreApi[]> {
-     return this.api.get('store')
-   }
+  public findAllAvailable(subscription: SubscriptionDelegate): Subscription {
 
-  findOne(bid: number): Observable<StoreApi> {
+    subscription = StoreSubscriptionFilters.filterOpen(subscription);
+    subscription = StoreSubscriptionFilters.filterActive(subscription);
+    subscription = StoreSubscriptionFilters.filterCanOrderOnline(subscription);
+    subscription = StoreSubscriptionFilters.filterHasDeliveryOrTakeaway(subscription);
+    subscription = StoreSubscriptionFilters.filterSendOrderByPushOrEmail(subscription);
+
+    return this.find().subscribe(subscription);
+  }
+
+
+  private find(): Observable<StoreApi[]> {
+    return this.api.get('store')
+  }
+
+  public findOne(bid: number): Observable<StoreApi> {
     return this.api.getOne('store', bid)
+  }
+
+}
+
+class StoreSubscriptionFilters {
+
+  public static filterOpen(subscription: SubscriptionDelegate): SubscriptionDelegate {
+    return a => subscription(a.filter(b => b.isOpen));
+  }
+
+  public static filterActive(subscription: SubscriptionDelegate): SubscriptionDelegate {
+    return a => subscription(a.filter(b => b.isActive));
+  }
+
+  public static filterCanOrderOnline(subscription: SubscriptionDelegate): SubscriptionDelegate {
+    return a => subscription(a.filter(b => b.canOrderOnline));
+  }
+
+  public static filterHasDeliveryOrTakeaway(subscription: SubscriptionDelegate): SubscriptionDelegate {
+    return a => subscription(a.filter(b => b.hasDelivery || b.hasTakeaway));
+  }
+
+  public static filterSendOrderByPushOrEmail(subscription: SubscriptionDelegate): SubscriptionDelegate {
+    return a => subscription(a.filter(b => b.sendOrderByPush || b.sendOrderByEmail));
   }
 
 }
