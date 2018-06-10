6622749003c107f964aeb994559649fa2e98d3c7
Nic-Kofo <nickkofo@gmail.com>
2018-06-10 13:39:34 +0300

    add sorting to store page

--- src/pages/store/store.ts
+++ src/pages/store/store.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StoreApi } from 'models/Api/Store';
 import { StoreProvider } from '../../providers/store/store';
+import { ProductApi } from '../../models/api/Product';
+import { List } from 'linqts';
 
 @IonicPage()
 @Component({
@@ -11,6 +13,7 @@ import { StoreProvider } from '../../providers/store/store';
 export class StorePage {
 	storeSegment: string = "catalog";
 	store: StoreApi;
+	categories: any[];
 
 	constructor(public navCtrl: NavController, public navParams: NavParams, public storeProvider: StoreProvider) {
 	}
@@ -25,6 +28,26 @@ export class StorePage {
 
 	getStore(): void {
 		var storeId = this.navParams.get('storeId');
-		this.storeProvider.findOne(storeId).subscribe(s => this.store = s);
+		this.storeProvider.findOne(storeId).subscribe(
+			s => {
+				this.store = s;
+				var p = new List<ProductApi>(s.Products);
+				p = p.Where(a => a.isActive);
+				p = p.Where(a => a.Product_Tags.filter(b => b.level === 2 || b.level === 3).length > 0);
+				p = p.OrderBy(a => a.orderNumber);
+				var categories = p.GroupBy(
+					a => a.Product_Tags
+						.filter(b => b.level === 2 || b.level === 3)
+						.sort(b => b.level === 3 ? 1 : b.level === 2 ? -1 : 0)[0].Tag.name, b => b
+					);
+				this.categories = Object.keys(categories).map(function(tagName){
+					let category = categories[tagName]
+						.sort((a, b) => a.orderNumber === null || (b.orderNumber !== null && a.orderNumber > b.orderNumber) ? 1 : -1);
+					category.key = tagName;
+					return category;
+				});
+				// console.log(this.categories);
+			}
+		);
 	}
 }
