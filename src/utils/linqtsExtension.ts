90bf9c90946dc427be0556080cb3c965aa7ae29f
Christos Talagozis <talagozis@hotmail.com>
2018-06-10 18:22:11 +0300

    Implemented linqtsExtension file.

---
+++ src/utils/linqtsExtension.ts
@@ -0,0 +1,15 @@
+import { List } from "linqts";
+
+export { }
+
+declare global {
+	interface Array<T> {
+		ToList(): List<T>;
+	}
+}
+
+if (!Array.prototype.ToList) {
+	Array.prototype.ToList = function () {
+		return new List<any>(this);
+	}
+}
