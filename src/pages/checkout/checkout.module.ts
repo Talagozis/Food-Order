845e936986ab3ab2ad88f34e2adc4e75eff78e04
Nic-Kofo <nickkofo@gmail.com>
2018-07-03 18:45:13 +0300

    progress on checkout page

--- src/pages/checkout/checkout.module.ts
+++ src/pages/checkout/checkout.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CheckoutPage } from './checkout';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { CheckoutPage } from './checkout';
   ],
   imports: [
     IonicPageModule.forChild(CheckoutPage),
+    PipesModule
   ],
 })
 export class CheckoutPageModule {}
