3a02250fe47e5970a955a5fc25ee3741606f48d3
Talagozis <talagozis8@gmail.com>
2019-01-28 14:43:39 +0200

    Adjust caching routing in service wroker.

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,8 +8,9 @@
 importScripts('./build/sw-toolbox.js');
 
 self.toolbox.options.cache = {
-	name: 'ionic-cache-v1901271754'
+	name: 'ionic-cache-v1901281143'
 };
+// self.toolbox.options.debug = true;
 
 // pre-cache our key assets
 self.toolbox.precache(
@@ -23,12 +24,21 @@ self.toolbox.precache(
 	]
 );
 
-// dynamically cache local index.html asset
-self.toolbox.router.any(/index.html$/, self.toolbox.fastest);
-self.toolbox.router.any(/^((?!^\/\?).)*$/mgi, self.toolbox.fastest);
+// post requests
+self.toolbox.router.post(/.*/, self.toolbox.networkOnly)
 
-// dynamically cache any other local assets
-self.toolbox.router.any(/^((?!index.html).)*$/mgi, self.toolbox.fastest);
+// get home page
+self.toolbox.router.get(/\/\?utm_source=a2hs$/mi, self.toolbox.networkOnly)
+
+// get api
+self.toolbox.router.get(/^.*\/api\/v.\/.*$/gmi, self.toolbox.networkOnly)
+
+// get google analytics
+self.toolbox.router.get(/^.*www\.google\-analytics\.com.*$/gmi, self.toolbox.networkOnly)
+
+// dynamically cache any local assets
+self.toolbox.router.get(/^.*(\.jpg|\.png).*$/, self.toolbox.fastest)
+self.toolbox.router.get(/^.*(\.js|\.css).*$/, self.toolbox.cacheFirst)
 
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
