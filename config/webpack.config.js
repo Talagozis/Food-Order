433e6d32eb3f5ff34a06ee5bc5daa1e81283dd0a
Talagozis <talagozis8@gmail.com>
2019-11-18 18:50:10 +0200

    Configured tslinter.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,21 +6,21 @@ var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
 var env = process.env.IONIC_ENV;
 
 useDefaultConfig[env].resolve.alias = {
-  "@app/env": path.resolve(environmentPath(env))
+	"@app/env": path.resolve(environmentPath(env))
 };
 
 function environmentPath(env) {
-  var envP =   env !== 'prod' && env !== 'dev' ? '' : '.' + env;
-  var filePath = './src/environments/environment' + envP + '.ts';
-  if (!fs.existsSync(filePath)) {
-    console.log(chalk.red('\n' + filePath + ' does not exist!'));
-  } else {
-    return filePath;
-  }
+	var envP = env !== 'prod' && env !== 'dev' ? '' : '.' + env;
+	var filePath = './src/environments/environment' + envP + '.ts';
+	if (!fs.existsSync(filePath)) {
+		console.log(chalk.red('\n' + filePath + ' does not exist!'));
+	} else {
+		return filePath;
+	}
 }
 
 module.exports = function () {
-  useDefaultConfig.prod.output['chunkFilename'] = "[name].[chunkhash].chunk.js";
-  useDefaultConfig.dev.output['chunkFilename'] = "[name].[chunkhash].chunk.js";
-  return useDefaultConfig;
+	useDefaultConfig.prod.output['chunkFilename'] = "[name].[chunkhash].chunk.js";
+	useDefaultConfig.dev.output['chunkFilename'] = "[name].[chunkhash].chunk.js";
+	return useDefaultConfig;
 };
